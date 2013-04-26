79565503eb56be436cd77546b656c5f463c15374
David Bosschaert <david.bosschaert@gmail.com>
2013-04-26 09:44:20 +0100

    Working test

--- test/microservices-inject-spec.js
+++ test/microservices-inject-spec.js
@@ -1,5 +1,20 @@
 describe("Microservices Inject", function() {
-	it("test", function() {
-		// works
+	it("Test Basic Injection", function() {
+		setupServiceX();
+		
+		var q = new Object();
+		q.inject = {inj: "myKey"};
+		xservices.injectServices(q);
+		expect(q.inj.doit()).toEqual("X");
+		
 	});
 });
+
+function setupServiceX() {
+	var serviceX = new Object();
+	serviceX.doit = function() {
+		return "X";
+	};
+	
+	xservices.registerService(serviceX, {myKey: "myValue"});	
+}
